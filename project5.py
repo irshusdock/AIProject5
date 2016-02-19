314300901b18f957c9fd85f47104bc40ea36134d
Ian Shusdock <ishusdock@gmail.com>
2016-02-19 12:06:28 -0500

    added input files, working on inputing data

--- project5.py
+++ project5.py
@@ -3,6 +3,12 @@
 
 import sys
 
+
+class Variable:
+	def __init__(self, name, weight):
+		self.name = name
+		self.weight = weight
+
 "Main script for the program"
 def project5_main():
 	
@@ -10,10 +16,22 @@ def project5_main():
 	file_content = f.readlines()
 	f.close()
 	
-	count = 0
+	file_content = file_content[1:]
+
+	variables = []
+	index = 0
+
 	for line in file_content:
 		if(line[0] == "#"):
-			count = count + 1
-		
+			break
+		variables.append(Variable(line[0], line[2]))
+
+
+
+
+
+
+if __name__ == '__main__':
+	project5_main()		
 	
 	
